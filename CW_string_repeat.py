ea123a876a52e616136b745ce5c286d71cba88d8
Wrobelax <awrobel.wr@gmail.com>
2025-03-19 21:38:34 +0100

    Add files via upload
    This is a script with a function for multiplying a string by n times.
    
    Solution of a Code Wars challenge "String repeat". 8 kyu

---
+++ CW_string_repeat.py
@@ -0,0 +1,15 @@
+def repeat_str(repeat:int, string:str) -> str:
+    """
+    Function for multiplying a string by n times.
+
+    Args:
+    :param repeat: Integer that will multiply a string
+    :param string: String of text to multiply
+
+    :return: String multiplied by repeat
+    """
+
+
+    new_string = string * repeat
+
+    return f'{new_string}'
