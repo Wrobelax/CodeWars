4aa5953e2f03a31389ed829022de3bef6a6ec825
Wrobelax <awrobel.wr@gmail.com>
2025-03-20 09:00:02 +0100

    Add files via upload
    This is a script with a function for returning string from integers.
    
    Solution of a Code Wars challenge "Convert a Number to a String!". 8 kyu

---
+++ CW_number_to_string.py
@@ -0,0 +1,11 @@
+def number_to_string(num:int) -> str:
+    """
+    Function for returning string from integers.
+
+    :param num: Integers.
+    :return: Strings.
+    """
+
+    return f'{num}'
+
+print(number_to_string(0))
