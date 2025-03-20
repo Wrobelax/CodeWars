7bed067aadb24b961c2fb1586453e5ca994fe8ce
Wrobelax <awrobel.wr@gmail.com>
2025-03-20 09:21:16 +0100

    Add files via upload
    This is a script with a function for returning a short greeting with a given name.
    
    Solution of a Code Wars challenge "Returning string". 8 kyu

---
+++ CW_returning_string.py
@@ -0,0 +1,9 @@
+def greet(name):
+    """
+    Function for returning a short greeting with a given name.
+    
+    :param name: String.
+    :return: String message with a name parameter.
+    """
+
+    return f"Hello, {name} how are you doing today?"
