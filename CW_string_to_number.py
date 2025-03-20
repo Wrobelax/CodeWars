22fb948eabe45ddc908dd7a965fa34ec876c11de
Wrobelax <awrobel.wr@gmail.com>
2025-03-20 09:03:56 +0100

    Add files via upload
    This is a script with a function for returning an integer from a string that is a valid integer.
    
    Solution of a Code Wars challenge "Convert a String to a Number!". 8 kyu

---
+++ CW_string_to_number.py
@@ -0,0 +1,8 @@
+def string_to_number(s):
+    """
+    Function for returning an integer from a string that is a valid integer.
+
+    :param s: String to be converted.
+    :return: Integer of a string.
+    """
+    return int(s)
